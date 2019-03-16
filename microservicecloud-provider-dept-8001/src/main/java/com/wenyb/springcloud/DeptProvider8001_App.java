24a6cf3723974f65f862d2fa663a72884ae166b4
wenyabing <yabingwen@163.com>
2019-03-16 15:26:25 +0800

    microservicecloud-provider-dept-8001 将已有的部门微服务注册进eureka服务中心

--- microservicecloud-provider-dept-8001/src/main/java/com/wenyb/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/wenyb/springcloud/DeptProvider8001_App.java
@@ -2,12 +2,14 @@ package com.wenyb.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author wenyabing
  * @Date 2019/3/15 9:31
  */
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
