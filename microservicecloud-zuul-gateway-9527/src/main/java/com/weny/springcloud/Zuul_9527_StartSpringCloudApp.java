524803b40d0dddc0761db1f954cf050d13f29d84
wenyabing <yabingwen@163.com>
2019-03-17 20:19:46 +0800

    添加zuul路由网关

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/weny/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,17 @@
+package com.weny.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author wenyabing
+ * @Date 2019/3/17 20:05
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
