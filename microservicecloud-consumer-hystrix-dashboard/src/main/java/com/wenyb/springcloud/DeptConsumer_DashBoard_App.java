87c2cdc76940c500e3c2d2b8740c43c6d43e9773
wenyabing <yabingwen@163.com>
2019-03-17 13:13:05 +0800

    添加准实时的调用监控（Hystrix Dashboard）

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/wenyb/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,17 @@
+package com.wenyb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author wenyabing
+ * @Date 2019/3/17 12:54
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class, args);
+    }
+}
